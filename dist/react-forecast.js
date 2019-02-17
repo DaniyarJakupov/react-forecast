8913178334d88f73dac6385a46ed3a58e0f24f63
Daniyar <ddr199305@gmail.com>
2019-02-17 18:26:55 +0100

    Fix optional params

--- dist/react-forecast.js
+++ dist/react-forecast.js
@@ -6,15 +6,15 @@ var createReactClass = require('create-react-class');
 module.exports = createReactClass({
   displayName: 'Forecast',
 
-  getDefaultProps: function getDefaultProps() {
+  getDefaultProps: function () {
     return {
       height: 245,
       width: '100%'
     };
   },
 
-  render: function render() {
-    var url = '//forecast.io/embed/#lat=' + this.props.latitude + '&lon=' + this.props.longitude + '&name=' + this.props.name || '' + '&color=' + this.props.color || '' + '&font=' + this.props.font || '' + '&units=' + this.props.unit || '';
+  render: function () {
+    var url = "//forecast.io/embed/#lat=" + this.props.latitude + "&lon=" + this.props.longitude + ("&name=" + this.props.name || '') + ("&color=" + this.props.color || '') + ("&font=" + this.props.font || '') + ("&units=" + this.props.units || '');
 
     return React.createElement('iframe', { type: 'text/html', height: this.props.height, width: this.props.width, frameBorder: '0', src: url });
   }
